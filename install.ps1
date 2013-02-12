1a08d55ae4430536f71b949e8a234e0c0b702542
Sam Williamson <samuel.g.williamson@gmail.com>
2013-02-12 07:44:27 -0500

    initial script

---
+++ install.ps1
@@ -0,0 +1,31 @@
+# Chocolatey
+#iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+# WEB PI
+cinst webpicommandline
+cinst IIS7 -source webpi
+cinst ASPNET -source webpi
+
+cinst 7zip.commandline
+cinst appharborcli
+cinst chocolateygui
+cinst curl
+cinst dropbox
+cinst fiddler
+cinst filezilla
+cinst filezilla.server
+cinst git
+cinst gitextensions
+cinst githubforwindows
+cinst heroku-toolbelt
+cinst javaruntime
+cinst jenkins
+cinst launch
+cinst linqpad4
+cinst nodejs
+cinst notepadplusplus
+cinst p4merge
+cinst paint.net
+cinst pidgin
+cinst sqliteadmin
+cinst winmerge
